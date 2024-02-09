2f006911744929ff909350b1493ae0ddbd23c037
Andreawthederp <81652601+andrewthederp@users.noreply.github.com>
2024-02-09 17:49:27 +0200

    Update modal_creator.py

--- utilities/discord_utilities/modal_creator.py
+++ utilities/discord_utilities/modal_creator.py
@@ -5,14 +5,17 @@ class MakeModal(discord.ui.Modal):
         super().__init__(title=title)
 
         for text_input_data in inputs:
-            text_input = discord.ui.TextInput(
-                label=text_input_data["label"],
-                placeholder=text_input_data["placeholder"],
-                required=text_input_data.get("required", False),
-                style=text_input_data.get("style", discord.TextStyle.short),
-                default=text_input_data.get('default')
-            )
-            self.add_item(text_input)
+            if isinstance(text_input_data, dict):
+                text_input = discord.ui.TextInput(
+                    label=text_input_data["label"],
+                    placeholder=text_input_data["placeholder"],
+                    required=text_input_data.get("required", False),
+                    style=text_input_data.get("style", discord.TextStyle.short),
+                    default=text_input_data.get('default')
+                )
+                self.add_item(text_input)
+            elif isinstance(text_input_data, discord.ui.TextInput):
+                self.add_item(text_input_data)
 
         self.on_submit_func = callback
 
