2417c19aeb49dbb6ae651348fef2fff532ea15cc
Andreawthederp <81652601+andrewthederp@users.noreply.github.com>
2024-02-11 11:29:26 +0200

    new function :D

--- utilities/discord_utilities/__init__.py
+++ utilities/discord_utilities/__init__.py
@@ -1,4 +1,11 @@
+import discord
+
 from .embed_creator import *
 from .modal_creator import *
 from .local_image_embed import *
-from.utils import get_image_url
+from .utils import get_image_url
+
+
+def convert_to_file(txt: str, filename: str):
+    import io
+    return discord.File(io.BytesIO(txt.encode('utf-8')), filename=filename)
