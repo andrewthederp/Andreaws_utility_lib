139c494c84b89eb095b515fa6407d531e32dcbc2
Andreawthederp <81652601+andrewthederp@users.noreply.github.com>
2024-02-11 06:41:49 +0200

    Update confirmation.py

--- utilities/discord_utilities/confirmation.py
+++ utilities/discord_utilities/confirmation.py
@@ -1,21 +1,39 @@
+import typing
+
 import discord
 
+
 class Confirm(discord.ui.View):
-    def __init__(self, *, user, timeout=None):
+    def __init__(self,
+                 *,
+                 user: typing.Union[discord.User, discord.Member],
+                 timeout: int = None,
+                 yes_callback: callable = None,
+                 no_callback: callable = None
+                 ):
         super().__init__(timeout=timeout)
         self.user = user
         self.answer = None
 
+        self.yes_callback = yes_callback
+        self.no_callback = no_callback
+
     @discord.ui.button(label='Yes', style=discord.ButtonStyle.green)
-    async def yes_button(self, interaction: discord.Interaction, _: discord.Button):
+    async def yes_button(self, interaction: discord.Interaction, button: discord.Button):
         self.stop()
         self.answer = True
 
-        await interaction.message.delete()
+        if self.yes_callback:
+            await self.yes_callback(interaction, button)
+        else:
+            await interaction.message.delete()
 
     @discord.ui.button(label='No', style=discord.ButtonStyle.red)
-    async def no_button(self, interaction: discord.Interaction, _: discord.Button):
+    async def no_button(self, interaction: discord.Interaction, button: discord.Button):
         self.stop()
         self.answer = False
 
-        await interaction.message.delete()
+        if self.no_callback:
+            await self.no_callback(interaction, button)
+        else:
+            await interaction.message.delete()
