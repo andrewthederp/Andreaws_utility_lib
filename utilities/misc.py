6b7610a7f9e83d58ea3b68c63b682d6104328a3a
Andreawthederp <81652601+andrewthederp@users.noreply.github.com>
2024-02-17 21:48:23 +0200

    improve

--- utilities/misc.py
+++ utilities/misc.py
@@ -1,13 +1,18 @@
 import asyncio
 import functools
+from inspect import isawaitable
 
 def run_in_executor():
     def decorator(func):
         @functools.wraps(func)
-        def wrapper(*args, **kwargs):
+        async def wrapper(*args, **kwargs):
             partial = functools.partial(func, *args, **kwargs)
             loop = asyncio.get_event_loop()
-            return loop.run_in_executor(None, partial)
+            out = loop.run_in_executor(None, partial)
+            if isawaitable(out):
+                return await out
+            else:
+                return out
 
         return wrapper
 
