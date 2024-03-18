ddc474f3fc609c354f2769eb1f545e23cdda50bb
Andreawthederp <81652601+andrewthederp@users.noreply.github.com>
2024-03-18 02:10:17 +0200

    paginator update

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ desc = (here / "README.md").read_text(encoding="utf-8")
 
 setup(
     name="andreaw_utilities",
-    version="0.0.30",
+    version="0.0.31",
     description="A library containing various utilities that I use in my projects.",
     long_description=desc,
     long_description_content_type="text/markdown",
