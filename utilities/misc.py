717a8f287e724458373697e34e5cbd610dacdb32
Andreawthederp <81652601+andrewthederp@users.noreply.github.com>
2024-09-04 08:32:02 +0200

    upgrade

--- utilities/misc.py
+++ utilities/misc.py
@@ -1,3 +1,4 @@
+from __future__ import annotations
 import asyncio
 import functools
 import typing
@@ -19,6 +20,23 @@ from typing import Optional, Iterable
 #         return wrapper
 #
 #     return decorator
+
+
+class TableFormat:
+    DOWN_RIGHT = "\u250F"
+    DOWN_HORIZONTAL = "\u2533"
+    DOWN_LEFT = "\u2513"
+
+    VERTICAL_RIGHT = "\u2523"
+    VERTICAL_HORIZONTAL = "\u254B"
+    VERTICAL_LEFT = "\u252B"
+
+    UP_RIGHT = "\u2517"
+    UP_HORIZONTAL = "\u253B"
+    UP_LEFT = "\u251B"
+
+    HORIZONTAL = "\u2501"
+    VERTICAL = "\u2503"
 
 
 def run_in_executor(func):
@@ -56,34 +74,64 @@ def get_global_variable(name):
 
 
 class Tabulate:
-    def __init__(self, *, columns: list[str]):
-        self.rows = [columns]
-        self.widths = [len(i) for i in columns]
+    def __init__(self, *, columns: list[str], table_format=TableFormat):
+        self.rows = [self._do_row_magic(columns)]
+        self.table_format = table_format
 
-    def add_row(self, row_data: list[str]):
-        if len(row_data) != (column_count := len(self.widths)):
+    @staticmethod
+    def _do_row_magic(row_data):
+        max_height = max(i.count("\n") for i in row_data) + 1
+        row_data = [row.split('\n') for row in row_data]
+
+        for i, x in enumerate(row_data):
+            if len(x) < max_height:
+                temp = [""] * max_height
+                start_index = (max_height - len(x)) // 2
+                for j in range(start_index, start_index + len(x)):
+                    temp[j] = x[j - start_index]
+                row_data[i] = temp
+
+        return row_data
+
+    def add_row(self, row_data: list[str]) -> Tabulate:
+        if len(row_data) != (column_count := len(self.rows[0])):
             raise ValueError(f"row data must be {column_count} columns big!")
 
+        row_data = self._do_row_magic(row_data)
         self.rows.append(row_data)
-        self.widths = [max(len(str(i)), self.widths[n]) for n, i in enumerate(row_data)]
+        return self
 
-    def format(self):
-        sep = "\u2533".join("\u2501" * (w + 2) for w in self.widths)
-        in_sep = "\u254B".join("\u2501" * (w + 2) for w in self.widths)
+    def format(self, table_format=None) -> str:
+        table_format: TableFormat | None = table_format or self.table_format
+        widths: list[int] = [max(max(len(section) for section in row[i]) for row in self.rows) for i in range(len(self.rows[0]))]
+        heights: list[int] = [max(len(section) for section in row) for row in self.rows]
 
-        string = "\u250F" + sep + "\u2513\n"
+        start_sep = table_format.DOWN_HORIZONTAL.join(table_format.HORIZONTAL * (w + 2) for w in widths)
+        in_sep = start_sep.replace(table_format.DOWN_HORIZONTAL, table_format.VERTICAL_HORIZONTAL)
+        end_sep = start_sep.replace(table_format.DOWN_HORIZONTAL, table_format.UP_HORIZONTAL)
+
+        string = table_format.DOWN_RIGHT + start_sep + table_format.DOWN_LEFT + "\n"
         for row_count, row in enumerate(self.rows, start=1):
-            string += "\u2503"
-            for n, cell in enumerate(row):
-                string += cell.center(self.widths[n] + 2)
-                string += "\u2503"
-            string += "\n"
+            height = heights[row_count - 1]
+
+            for i in range(height):
+                string += table_format.VERTICAL
+
+                for n, cell in enumerate(row):
+                    string += cell[i].center(widths[n] + 2)
+
+                    string += table_format.VERTICAL
+
+                string += "\n"
 
             if row_count == len(self.rows):
-                string += "\u2517" + sep.replace("\u2533", "\u253B") + "\u251B"
+                string += table_format.UP_RIGHT + end_sep + table_format.UP_LEFT
             else:
-                string += "\u2523" + in_sep + "\u252B"
+                string += table_format.VERTICAL_RIGHT + in_sep + table_format.VERTICAL_LEFT
 
             string += "\n"
 
         return string
+
+    def __str__(self):
+        return self.format()
