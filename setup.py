ad49c9cfb3e5bf341baed11dd3d75b542ce91e11
Andreawthederp <81652601+andrewthederp@users.noreply.github.com>
2024-12-22 00:58:40 +0200

    bump version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ desc = (here / "README.md").read_text(encoding="utf-8")
 
 setup(
     name="andreaw_utilities",
-    version="0.0.67",
+    version="0.0.69",
     description="A library containing various utilities that I use in my projects.",
     long_description=desc,
     long_description_content_type="text/markdown",
--- utilities/commands/__init__.py
+++ utilities/commands/__init__.py
@@ -1,4 +1,4 @@
-from .command import Command, command, add_command, remove_command, get_command_list, process_commands, async_process_commands
+from .command import Command, command, add_command, remove_command, get_command_list, process_commands
 from .converter import Converter, convert, async_convert
 from .parameter import Parameter
 from .view import StringView
